257dd6246ebc85e7511eca0ec425705d7d4bb5f1
Morgan Rolitrø Sjømark <morgan.sjomark@gmail.com>
2021-06-24 23:25:30 +0200

    Updated phone menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,15 @@
-import * as React from "react"
+import React, {useState} from "react"
 import { Link } from "gatsby"
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { StaticImage } from "gatsby-plugin-image";
 
 import Logo from "../images/logo.png";
+import Menubar from "../images/bars-solid.svg";
 
 const HeaderWrapper = styled.header`
   background-color: #f7f7f7;
+  position: relative;
+  z-index: 10;
 `;
 
 const HeaderContent = styled.div`
@@ -35,10 +38,22 @@ const Menu = styled.div`
   text-align: right;
   display: flex;
   flex-direction: row-reverse;
+  transition: opacity 0.5s;
+  z-index: -1;
   @media (max-width: 768px) {
-    display: block;
-    margin-top: 20px;
+    position: absolute;
+    top: -70px;
+    left:0;
+    right: 0;
+    background-color: rgba(255,255,255,0.9);
     text-align: center;
+    display: block;
+    padding: 15px 0;
+    opacity: 0;
+    ${p => p.active && css`
+      opacity: 1;
+      top: 70px;
+    `}
   }
 `;
 
@@ -46,26 +61,48 @@ const MenuItem = styled(Link)`
   text-align: center;
   padding: 0 20px;
   box-sizing: border-box;
-  color: #0056b8;
   text-decoration: none;
   font-size: 16px;
   font-weight: 500;
+  color: #000000cc;
+  transition: all 0.2s;
+  &:hover{
+    color: #0056b8;
+  }
 `;
 
-const Header = () => (
+const MenuToggle = styled.img`
+  position: absolute;
+  top: 23px;
+  right: 25px;
+  height: 25px;
+  width: 25px;
+  display: none;
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
+
+const Header = () => {
+  const [open, setOpen] = useState(false)
+  return(
   <HeaderWrapper>
     <HeaderContent>
       <Link to="/"><img 
       src={Logo}
       alt="Logo" />
       </Link>
-      <Menu>
-        <MenuItem to={"/#kontakt"}>Kontakt</MenuItem>
+      <MenuToggle onClick={()=>{
+        setOpen(!open);
+      }} src={Menubar} />
+      <Menu active={open} >
+        <MenuItem  to={"/#kontakt"}>Kontakt</MenuItem>
         <MenuItem to={"/#tjenester"}>Tjenester</MenuItem>
         <MenuItem to={"/#hjem"}>Hjem</MenuItem>
       </Menu>
     </HeaderContent>
   </HeaderWrapper>
-)
+  );
+}
 
 export default Header
